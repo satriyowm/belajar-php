bc8cb9b0b0de7f0f7d90f8d72db8603d3085e121
Satriyo Watana Ma'arif <72974204+satriyowm@users.noreply.github.com>
2022-12-22 18:16:31 +0700

    Add files via upload

---
+++ crud.php
@@ -0,0 +1,66 @@
+<?php
+    // 1. Buat koneksi dengan MySQL
+    $con = mysqli_connect("localhost","root","","universitas");
+
+    // 2. Cek koneksi dengan MySQL
+    if(mysqli_connect_errno()){
+        echo "Koneksi gagal". mysqli_connect_error();
+    }else{
+        echo "Koneksi berhasil";
+    }
+
+    // 3. Membaca data dari table mysql.
+    $query = "SELECT * FROM data_mahasiswa";
+    
+    // 4. tampilkan data, dengan menjalankan sql query
+    $result = mysqli_query($con,$query);
+    $data_mahasiswa = [];
+    if ($result){
+        // tampilkan data satu per satu
+        while($row = mysqli_fetch_assoc($result)){
+            $data_mahasiswa[] = $row;
+        }
+        mysqli_free_result($result);
+    }
+
+    // 5. tutup koneksi mysql
+    mysqli_close($con);
+    
+    ?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Data Mahasiswa</title>
+    </head>
+    <body>
+        <h1>Data Mahasiswa</h1>
+        <a href="insert.php">Tambah Data</a>
+        <table border="1" style="width:100%;">
+            <tr>
+                <th>NIM</th>
+                <th>Nama</th>
+                <th>Jenis Kelamin</th>
+                <th>Tempat Lahir</th>
+                <th>Tanggal Lahir</th>
+                <th>Alamat</th>
+                <th>Action</th>
+            </tr>
+            <?php foreach($mahasiswa as $value): ?>
+            <tr>
+                <td><?php echo $value["nim"]; ?></td>
+                <td><?php echo $value["nama"]; ?></td>
+                <td><?php echo $value["jenis_kelamin"]; ?></td>
+                <td><?php echo $value["tempat_lahir"]; ?></td>
+                <td><?php echo $value["tanggal_lahir"]; ?></td>
+                <td><?php echo $value["alamat"]; ?></td>
+                    <a href="<?php echo "update.php?id=".$value["id"]; ?>">Edit</a>
+                    <a href="<?php echo "delete.php?id=".$value["id"]; ?>">Delete</a>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+    </body>
+    </html>
